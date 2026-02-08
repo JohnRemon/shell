a203177164b40ca4d8972fc697e1a3286ddb4b58
JohnRemon <john1812008@gmail.com>
2026-02-08 20:06:26 +0100

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,21 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
+    public static List<Path> PATH;
+
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         do {
             System.out.print("$ ");
             String command = sc.next();
             String arguments = sc.nextLine().trim();
+            addPath(System.getenv("PATH"));
 
             switch (command) {
                 case "exit" -> exit();
@@ -14,7 +23,6 @@ public class Main {
                 case "type" -> type(arguments);
                 default -> System.out.println(command + ": command not found");
             }
-            ;
 
         } while (true);
     }
@@ -28,11 +36,40 @@ public class Main {
     }
 
     private static void type(String arguments) {
-        if (arguments.equals("echo") || arguments.equals("exit") | arguments.equals("type")) {
+        if (isBuiltin(arguments)) {
             System.out.println(arguments + " is a shell builtin");
+        } else if (isExecutable(arguments)) {
+            Path path = getPath(arguments);
+            System.out.println(arguments + " is " + path);
         } else {
             System.out.println(arguments + ": not found");
         }
     }
 
+    private static boolean isExecutable(String arguments) {
+        return getPath(arguments) != null;
+    }
+
+    private static Path getPath(String arguments) {
+        for (Path path : PATH) {
+            if (Files.isExecutable(path) && path.endsWith(arguments)) {
+                return path;
+            }
+        }
+        return null;
+    }
+
+    private static void addPath(String arguments) {
+        String[] directories = arguments.split(File.pathSeparator);
+        List<Path> paths = Arrays.stream(directories)
+                .map(Path::of)
+                .collect(Collectors.toList());
+
+        PATH.addAll(paths);
+
+    }
+
+    private static boolean isBuiltin(String arguments) {
+        return arguments.equals("exit") || arguments.equals("type") || arguments.equals("echo");
+    }
 }
