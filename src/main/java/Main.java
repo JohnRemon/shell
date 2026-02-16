c1615ac27266d1e0ca63be396fb3351960777656
JohnRemon <john1812008@gmail.com>
2026-02-16 18:54:32 +0100

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
 import java.util.List;
-import java.util.Scanner;
+
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.reader.impl.DefaultParser;
+import org.jline.reader.impl.completer.StringsCompleter;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
 
 import commands.Shell;
 
@@ -7,11 +13,24 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         Shell shell = new Shell();
-        Scanner sc = new Scanner(System.in);
+
+        Terminal terminal = TerminalBuilder.builder()
+                .system(true)
+                .build();
+
+        DefaultParser parser = new DefaultParser();
+        parser.setEscapeChars(null);
+
+        StringsCompleter completer = new StringsCompleter("echo", "exit");
+
+        LineReader reader = LineReaderBuilder.builder()
+                .terminal(terminal)
+                .parser(parser)
+                .completer(completer)
+                .build();
 
         while (true) {
-            System.out.print("$ ");
-            String input = sc.nextLine();
+            String input = reader.readLine("> ");
 
             if (input.isEmpty()) {
                 continue;
